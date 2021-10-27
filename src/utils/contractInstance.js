c687b7de3dd861035050495466e0427706327778
ZZ <490396674@qq.com>
2021-10-28 01:35:58 +0800

    usd deposit

--- src/utils/contractInstance.js
+++ src/utils/contractInstance.js
@@ -5,6 +5,8 @@ import { getAddress, getChainId } from "./contractAddress";
 
 // import abis
 import Degis_abi from "../abi/DegisToken.json";
+import MockUSD_abi from "../abi/MockUSD.json";
+import InsurancePool_abi from "../abi/InsurancePool.json";
 
 export const getCurrentAccount = async () => {
   return getWeb3().then((res) => res.currentProvider.selectedAddress);
@@ -18,3 +20,21 @@ export const getDegis = async () => {
     getAddress("DEGIS", chainId)
   );
 };
+
+export const getMockUSD = async () => {
+  const web3 = await getWeb3();
+  const chainId = getChainId();
+  return await new web3.eth.Contract(
+    MockUSD_abi.abi,
+    getAddress("MOCKUSD", chainId)
+  );
+}
+
+export const getInsurancePool = async () => {
+  const web3 = await getWeb3();
+  const chainId = getChainId();
+  return await new web3.eth.Contract(
+    InsurancePool_abi.abi,
+    getAddress("INSURANCEPOOL", chainId)
+  );
+}
--- src/views/Mining.vue
+++ src/views/Mining.vue
@@ -1,69 +1,15 @@
 <template>
   <base-header type="" class="pb-6 pb-8 pt-4">
-    <h1 class="fw-7 d-g1 fs-34 mb-4">Mining by DEGIS</h1>
+    <h1 class="big-head mt-2 mb-4">Mining by DEGIS</h1>
     <stats-card>
       <div class="container-fluid">
         <div class="row align-items-center">
           <div class="col-xl-6 order-md-1">
-            <h2 class="fw-7 d-g1 fs-28" style="padding: 3% 0">
-              The Miserable Flight Pool
-            </h2>
+            <h2 class="sub-head">The Miserable Flight Pool</h2>
             <div class="row justify-content-between" style="padding: 2% 0">
               <div class="col-6">
                 <img
                   src="img/function/mining-circle.png"
-                  class="img-fluid"
-                  style="width: 80%"
-                />
-              </div>
-              <div class="col-6">
-                <h5 class="text-l">Total Value Locker: <bold> XXXX</bold></h5>
-                <h5 class="text-l">Available Capacity: <bold> XXXX</bold></h5>
-                <h5 class="text-l">Current Policies: <bold> XXXX</bold></h5>
-                <h5 class="text-l">Locked Ratio: <bold> XXXX</bold></h5>
-                <h5 class="text-l">APR: <bold> XX%</bold></h5>
-              </div>
-            </div>
-            <h5 class="text-l">
-              Pool Address: <bold> XXXXXXXXXXXXXXXXXX</bold>
-            </h5>
-          </div>
-          <div class="col-xl-6 order-md-2">
-            <h5 class="text-r">Your Asset: <bold> XXXX (+XX%)</bold></h5>
-            <input
-              class="degis-input"
-              style="width: 100%; margin: 4% 0"
-              placeholder="0"
-            />
-            <div
-              class="d-flex justify-content-between"
-              style="padding-bottom: 11%"
-            >
-              <base-button style="width: 45%" @click="depositUSD">
-                DEPOSIT
-              </base-button>
-              <base-button style="width: 45%"> WITHDRAW </base-button>
-            </div>
-            <h5 class="text-r">Your Premium Income: <bold> XXXX</bold></h5>
-            <h5 class="text-r">Your DEGIS Token Income: <bold> XXXX</bold></h5>
-            <base-button style="width: 100%; margin-bottom: 2%"
-              >HARVEST REWARD</base-button
-            >
-          </div>
-        </div>
-      </div>
-    </stats-card>
-    <stats-card>
-      <div class="container-fluid">
-        <div class="row align-items-center">
-          <div class="col-xl-6 order-md-1">
-            <h2 class="fw-7 d-g1 fs-28" style="padding: 3% 0">
-              The Naughty Price Pool
-            </h2>
-            <div class="row justify-content-between" style="padding: 2% 0">
-              <div class="col-6">
-                <img
-                  src="img/function/mining-circle-2.png"
                   class="img-fluid"
                   style="width: 80%"
                 />
@@ -96,9 +42,53 @@
             </div>
             <h5 class="text-r">Your Premium Income: <bold> XXXX</bold></h5>
             <h5 class="text-r">Your DEGIS Token Income: <bold> XXXX</bold></h5>
-            <base-button style="width: 100%; margin-bottom: 2%"
-              >HARVEST REWARD</base-button
+            <base-button style="width: 100%; margin-bottom: 2%">HARVEST REWARD</base-button>
+          </div>
+        </div>
+      </div>
+    </stats-card>
+    <stats-card>
+      <div class="container-fluid">
+        <div class="row align-items-center">
+          <div class="col-xl-6 order-md-1">
+            <h2 class="sub-head">The Naught Price Pool</h2>
+            <div class="row justify-content-between" style="padding: 2% 0">
+              <div class="col-6">
+                <img
+                  src="img/function/mining-circle-2.png"
+                  class="img-fluid"
+                  style="width: 80%"
+                />
+              </div>
+              <div class="col-6">
+                <h5 class="text-l">Total Value Locker: <bold> XXXX</bold></h5>
+                <h5 class="text-l">Available Capacity: <bold> XXXX</bold></h5>
+                <h5 class="text-l">Current Policies: <bold> XXXX</bold></h5>
+                <h5 class="text-l">Locked Ratio: <bold> XXXX</bold></h5>
+                <h5 class="text-l">APR: <bold> XX%</bold></h5>
+              </div>
+            </div>
+            <h5 class="text-l">
+              Pool Address: <bold> XXXXXXXXXXXXXXXXXX</bold>
+            </h5>
+          </div>
+          <div class="col-xl-6 order-md-2">
+            <h5 class="text-r">Your Asset: <bold> XXXX (+XX%)</bold></h5>
+            <input
+              class="degis-input"
+              style="width: 100%; margin: 4% 0"
+              placeholder="0"
+            />
+            <div
+              class="d-flex justify-content-between"
+              style="padding-bottom: 11%"
             >
+              <deposit style="width: 45%"> DEPOSIT </deposit>
+              <base-button style="width: 45%"> WITHDRAW </base-button>
+            </div>
+            <h5 class="text-r">Your Premium Income: <bold> XXXX</bold></h5>
+            <h5 class="text-r">Your DEGIS Token Income: <bold> XXXX</bold></h5>
+            <base-button style="width: 100%; margin-bottom: 2%">HARVEST REWARD</base-button>
           </div>
         </div>
       </div>
@@ -107,54 +97,52 @@
 </template>
 
 <script>
-import BaseButton from "@/components/BaseButton";
-import Web3 from "web3";
-import { getMockUSD, getInsurancePool } from "../../utils/contractInstance";
+import BaseButton from "../components/BaseButton";
+import Deposit from "../components/Deposit";
+
 export default {
   name: "mining",
   components: {
     BaseButton,
-  },
-  methods: {
-    async depositUSD() {
-      // const degis = await getDegis();
-      // const account = this.$store.state.selectedAccount;
-      // const balance = await degis.methods.balanceOf(account).call();
-      // this.degisBalance = balance / 1e18;
-
-      const MockUSD = await getMockUSD();
-      const InsurancePool = await getInsurancePool();
-      const account = this.$store.state.selectedAccount;
-      
-      const deposit_amount = 10;
-      let f_amount = window.WEB3.utils.toWei(String(deposit_amount), "ether");
-
-      const tx1 = await MockUSD.methods
-        .approve(
-          InsurancePool.options.address,
-          window.WEB3.utils.toBN(f_amount)
-        )
-        .send({ from: account });
-      console.log("Tx Hash:", tx1.tx);
-
-      const tx2 = await InsurancePool.methods
-        .stake(account, window.WEB3.utils.toBN(f_amount))
-        .send({ from: account });
-      console.log("Tx Hash:", tx2.tx);
-    },
+    Deposit,
   },
 };
 </script>
+
 <style scoped>
+.big-head {
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 900;
+  font-size: 34px;
+  line-height: 36px;
+  letter-spacing: -0.02em;
+  color: #2c2d2f;
+}
+
+.sub-head {
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 28px;
+  color: #2c2d2f;
+  padding: 3% 0;
+}
+
 .text-l {
+  font-family: Montserrat;
+  font-style: normal;
   font-weight: normal;
   font-size: 14px;
   line-height: 22px;
   color: #86898c;
+
   padding: 4% 0;
 }
 
 .text-r {
+  font-family: Montserrat;
+  font-style: normal;
   font-weight: normal;
   font-size: 16px;
   color: #86898c;
