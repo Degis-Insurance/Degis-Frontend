c7cfed14c021489f9ee8929c6df63a67b59da9d4
Pengze <pchenax@connect.ust.hk>
2021-11-12 00:26:38 +0800

    #

--- src/views/Account/Account.vue
+++ src/views/Account/Account.vue
@@ -2,55 +2,58 @@
   <base-header type="" class="pb-6 pb-8 pt-4">
     <h1 class="fw-7 d-g1 fs-34 mb-4">Policies I Bought / Sold</h1>
 
-    <el-tabs type="" :stretch="true" @tab-click="handleClick">
+    <el-card>
+      <el-tabs type="" :stretch="true">
 
-      <el-tab-pane label="Policies I Created">
-        <template #label><p class="fw-7 d-g3 fs-16">Policies I Created</p></template>
-        <el-table :data="tableData" class="dg-table" header-cell-style="text-align: center; height: 70px" cell-style="text-align: center; height: 70px">
-          <el-table-column prop="airline" label="AIRLINE" sortable/>
-          <el-table-column prop="flightno" label="FLIGHT NO." sortable/>
-          <el-table-column prop="route" label="ROUTE" sortable/>
-          <el-table-column prop="departtime" label="DEPART TIME" sortable/>
-          <el-table-column prop="arrivetime" label="ARRIVE TIME" sortable/>
-          <el-table-column prop="premium" label="PREMIUM" sortable/>
-        </el-table>
+        <el-tab-pane label="Policies I Created">
+          <template #label><p class="fw-7 d-g3 fs-16">Policies I Created</p></template>
+          <el-table :data="tableData" class="dg-cardtable" header-cell-style="text-align: center; height: 70px" cell-style="text-align: center; height: 70px">
+            <el-table-column prop="airline" label="AIRLINE" sortable/>
+            <el-table-column prop="flightno" label="FLIGHT NO." sortable/>
+            <el-table-column prop="route" label="ROUTE" sortable/>
+            <el-table-column prop="departtime" label="DEPART TIME" sortable/>
+            <el-table-column prop="arrivetime" label="ARRIVE TIME" sortable/>
+            <el-table-column prop="premium" label="PREMIUM" sortable/>
+          </el-table>
 
-      </el-tab-pane>
-      <el-tab-pane>
-        <template #label><p class="fw-7 d-g3 fs-16">Bought</p></template>
-        <el-table :data="tableData" class="dg-table" header-cell-style="text-align: center; height: 70px" cell-style="text-align: center; height: 70px">
-          <el-table-column prop="airline" label="AIRLINE" sortable/>
-          <el-table-column prop="flightno" label="FLIGHT NO." sortable/>
-          <el-table-column prop="route" label="ROUTE" sortable/>
-          <el-table-column prop="departtime" label="DEPART TIME" sortable/>
-          <el-table-column prop="arrivetime" label="ARRIVE TIME" sortable/>
-          <el-table-column prop="premium" label="PREMIUM" sortable/>
-        </el-table>
-      </el-tab-pane>
+        </el-tab-pane>
+        <el-tab-pane>
+          <template #label><p class="fw-7 d-g3 fs-16">Bought</p></template>
+          <el-table :data="tableData" class="dg-cardtable" header-cell-style="text-align: center; height: 70px" cell-style="text-align: center; height: 70px">
+            <el-table-column prop="airline" label="AIRLINE" sortable/>
+            <el-table-column prop="flightno" label="FLIGHT NO." sortable/>
+            <el-table-column prop="route" label="ROUTE" sortable/>
+            <el-table-column prop="departtime" label="DEPART TIME" sortable/>
+            <el-table-column prop="arrivetime" label="ARRIVE TIME" sortable/>
+            <el-table-column prop="premium" label="PREMIUM" sortable/>
+          </el-table>
+        </el-tab-pane>
 
-      <el-tab-pane>
-        <template #label><p class="fw-7 d-g3 fs-16">SOLD</p></template>
-        <el-table :data="tableData" class="dg-table" header-cell-style="text-align: center; height: 70px" cell-style="text-align: center; height: 70px">
-          <el-table-column prop="airline" label="AIRLINE" sortable/>
-          <el-table-column prop="flightno" label="FLIGHT NO." sortable/>
-          <el-table-column prop="route" label="ROUTE" sortable/>
-          <el-table-column prop="departtime" label="DEPART TIME" sortable/>
-          <el-table-column prop="arrivetime" label="ARRIVE TIME" sortable/>
-          <el-table-column prop="premium" label="PREMIUM" sortable/>
-        </el-table>
-      </el-tab-pane>
-      <el-tab-pane>
-        <template #label><p class="fw-7 d-g3 fs-16">My Provision</p></template>
-        <el-table :data="tableData" class="dg-table" header-cell-style="text-align: center; height: 70px" cell-style="text-align: center; height: 70px">
-          <el-table-column prop="airline" label="AIRLINE" sortable/>
-          <el-table-column prop="flightno" label="FLIGHT NO." sortable/>
-          <el-table-column prop="route" label="ROUTE" sortable/>
-          <el-table-column prop="departtime" label="DEPART TIME" sortable/>
-          <el-table-column prop="arrivetime" label="ARRIVE TIME" sortable/>
-          <el-table-column prop="premium" label="PREMIUM" sortable/>
-        </el-table>
-      </el-tab-pane>
-    </el-tabs>
+        <el-tab-pane>
+          <template #label><p class="fw-7 d-g3 fs-16">SOLD</p></template>
+          <el-table :data="tableData" class="dg-cardtable" header-cell-style="text-align: center; height: 70px" cell-style="text-align: center; height: 70px">
+            <el-table-column prop="airline" label="AIRLINE" sortable/>
+            <el-table-column prop="flightno" label="FLIGHT NO." sortable/>
+            <el-table-column prop="route" label="ROUTE" sortable/>
+            <el-table-column prop="departtime" label="DEPART TIME" sortable/>
+            <el-table-column prop="arrivetime" label="ARRIVE TIME" sortable/>
+            <el-table-column prop="premium" label="PREMIUM" sortable/>
+          </el-table>
+        </el-tab-pane>
+        <el-tab-pane>
+          <template #label><p class="fw-7 d-g3 fs-16">My Provision</p></template>
+          <el-table :data="tableData" class="dg-cardtable" header-cell-style="text-align: center; height: 70px" cell-style="text-align: center; height: 70px">
+            <el-table-column prop="airline" label="AIRLINE" sortable/>
+            <el-table-column prop="flightno" label="FLIGHT NO." sortable/>
+            <el-table-column prop="route" label="ROUTE" sortable/>
+            <el-table-column prop="departtime" label="DEPART TIME" sortable/>
+            <el-table-column prop="arrivetime" label="ARRIVE TIME" sortable/>
+            <el-table-column prop="premium" label="PREMIUM" sortable/>
+          </el-table>
+        </el-tab-pane>
+      </el-tabs>
+
+    </el-card>
   </base-header>
 </template>
 
