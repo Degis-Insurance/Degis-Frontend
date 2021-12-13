bf4ca7b80862119aeaf6adc3eca3b2f91d81a65b
liyang-iMac <ylikp@connect.ust.hk>
2021-12-13 19:25:50 +0800

    change notification in flight provide

--- src/views/Flight/FlightProvide.vue
+++ src/views/Flight/FlightProvide.vue
@@ -58,12 +58,17 @@
               v-model="amount"
             />
             <div class="d-flex justify-content-between">
-              <base-button style="width: 45%" @click="depositUSDEvent">DEPOSIT</base-button>
-              <p> </p>
-<!--              <div>-->
-                <base-button @click="withdrawUSDEvent">WITHDRAW (7days locked)</base-button>
-<!--/*                <p class="fw-4 d-g2 fs-16"><span style="color:red">*</span>7 Days Locked</p>*/-->
-<!--              </div>-->
+              <base-button style="width: 45%" @click="depositUSDEvent"
+                >DEPOSIT</base-button
+              >
+              <p></p>
+              <!--              <div>-->
+              <base-button @click="withdrawUSDEvent"
+                >WITHDRAW (Currently 20 hours frozen time, 7days when
+                mainnet)</base-button
+              >
+              <!--/*                <p class="fw-4 d-g2 fs-16"><span style="color:red">*</span>7 Days Locked</p>*/-->
+              <!--              </div>-->
             </div>
             <!-- <h5 class="text-r">Your Premium Income: <bold> {{ userPendingDegis }}</bold></h5> -->
             <!-- <h5 class="text-r">Your DEGIS Token Income: <bold> {{ userPendingDegis }}</bold></h5> -->
--- src/views/Flight/OrderConfirm.vue
+++ src/views/Flight/OrderConfirm.vue
@@ -1,37 +1,42 @@
 <template>
   <div
-      class="modal fade"
-      @click.self="closeModal"
-      :class="[{ 'show d-block': show }, { 'd-none': !show }]"
-      v-show="show"
-      tabindex="-1"
-      role="dialog"
+    class="modal fade"
+    @click.self="closeModal"
+    :class="[{ 'show d-block': show }, { 'd-none': !show }]"
+    v-show="show"
+    tabindex="-1"
+    role="dialog"
   >
     <div class="modal-dialog modal-dialog-centered" style="max-width: 100%">
-      <div class="modal-content" style="width: 70%; border-radius: 24px; margin: auto">
-
+      <div
+        class="modal-content"
+        style="width: 70%; border-radius: 24px; margin: auto"
+      >
         <div class="modal-header pl-4 pt-4 pb-2">
           <h1 class="fw-7 d-g1 fs-24">Order Confirm</h1>
           <button class="close pr-4" @click="closeModal">
-            <img src="img/luckybox/close-modal.png" style="width: 24px"/>
+            <img src="img/luckybox/close-modal.png" style="width: 24px" />
           </button>
         </div>
 
-        <img src="img/luckybox/modal-split.png" style="height: 1px"/>
+        <img src="img/luckybox/modal-split.png" style="height: 1px" />
 
         <div class="row align-items-center pb-4">
           <div class="col-xl-5 pt-4" style="padding: 0 4%">
             <div class="d-flex pb-5">
-              <img src="img/function/flight-comp.png" style="width: 88px; height: 88px"/>
+              <img
+                src="img/function/flight-comp.png"
+                style="width: 88px; height: 88px"
+              />
               <div>
                 <p class="fw-4 d-g1 fs-20 pl-3 pt-2">{{ buyData.airline }}</p>
                 <p class="fw-4 d-g2 fs-14 pl-3">{{ buyData.flight_no }}</p>
               </div>
             </div>
             <img
-                src="img/function/flight-info.png"
-                class="pb-4"
-                style="width: 100%"
+              src="img/function/flight-info.png"
+              class="pb-4"
+              style="width: 100%"
             />
             <div class="d-flex justify-content-between">
               <p class="fw-4 d-p fs-32">{{ getFrom }}</p>
@@ -58,7 +63,7 @@
             <div>
               <p class="fw-4 d-g1 fs-20">Payout Mechanism</p>
               <div class="d-flex justify-content-center">
-                <img src="img/function/flight-pm.png" style="width: 80%"/>
+                <img src="img/function/flight-pm.png" style="width: 80%" />
               </div>
             </div>
 
@@ -70,11 +75,13 @@
                 </div>
                 <div class="d-flex justify-content-between">
                   <p class="fw-4 d-g2 fs-16">Maximum Payoff:</p>
-                  <p class="fw-7 d-p fs-16"> $120 </p>
+                  <p class="fw-7 d-p fs-16">$120</p>
                 </div>
               </div>
               <div class="col-xl-6 pt-1" align="right">
-                <base-button style="width: 70%" @click="NewPolicyEvent">CONFIRM</base-button>
+                <base-button style="width: 70%" @click="NewPolicyEvent"
+                  >CONFIRM</base-button
+                >
               </div>
             </div>
           </div>
@@ -91,7 +98,7 @@ import {
   getInsurancePool,
   getFLPolicyToken,
 } from "../../utils/contractInstance";
-import {getSignature} from "@/api/functions";
+import { getSignature } from "@/api/functions";
 export default {
   name: "order-confirm",
   components: {},
@@ -121,13 +128,12 @@ export default {
 
     async NewPolicy(timestamp, flight_no) {
       const account = this.$store.state.selectedAccount;
-      if(account == null)
-      {
-        alert("Please Connect Wallet")
-        return
-      } 
-      const info = await getSignature(String(account),flight_no,timestamp)
-      console.log("info",info.data.detail)
+      if (account == null) {
+        alert("Please Connect Wallet");
+        return;
+      }
+      const info = await getSignature(String(account), flight_no, timestamp);
+      console.log("info", info.data.detail);
 
       const policyFlow = await getPolicyFlow();
       const usd = await getMockUSD();
@@ -136,7 +142,10 @@ export default {
       const allowance = await usd.methods
         .allowance(account, insurancePool.options.address)
         .call();
-      if (parseInt(allowance) < parseInt(window.WEB3.utils.toWei("100000000", "ether"))) {
+      if (
+        parseInt(allowance) <
+        parseInt(window.WEB3.utils.toWei("100000000", "ether"))
+      ) {
         const tx1 = await usd.methods
           .approve(
             insurancePool.options.address,
@@ -147,43 +156,51 @@ export default {
           .send({ from: account });
         console.log("Tx Hash:", tx1.transactionHash);
       }
-      
+
       const productId = window.WEB3.utils.toBN(String(0));
-      const flight_number =  info.data.detail["flight_no"]
-      const premium = window.WEB3.utils.toBN(String(info.data.detail["premium"]))
-      const departure_timestamp =  window.WEB3.utils.toBN(String(info.data.detail["timestamp"]))
+      const flight_number = info.data.detail["flight_no"];
+      const premium = window.WEB3.utils.toBN(
+        String(info.data.detail["premium"])
+      );
+      const departure_timestamp = window.WEB3.utils.toBN(
+        String(info.data.detail["timestamp"])
+      );
       const landing_timestamp = window.WEB3.utils.toBN(String(0));
-      const deadline = window.WEB3.utils.toBN(String(info.data.detail["deadline"]))
-      const signature = info.data.detail["signature"]
+      const deadline = window.WEB3.utils.toBN(
+        String(info.data.detail["deadline"])
+      );
+      const signature = info.data.detail["signature"];
+
+      const now = new Date().getTime();
+      if (departure_timestamp - parseInt(now / 1000) < 86400) {
+        alert("The departure time is too close, please select another flight");
+      }
       // const policyToken = await getFLPolicyToken();
       // await policyToken.methods.updatePolicyFlow(policyFlow.options.address).send({"from":account})
       // await insurancePool.methods.setPolicyFlow(policyFlow.options.address).send({"from":account})
       const tx = await policyFlow.methods
         .newApplication(
-          productId, 
-          flight_number, 
-          premium, 
-          departure_timestamp, 
-          landing_timestamp, 
-          deadline, 
+          productId,
+          flight_number,
+          premium,
+          departure_timestamp,
+          landing_timestamp,
+          deadline,
           signature
         )
-        .send({from: account});
+        .send({ from: account });
       console.log("Tx Hash:", tx.transactionHash);
       console.log(tx);
       console.log("policy Id:", tx.logs[0].args[0]);
     },
 
     async NewPolicyEvent() {
-      
-      console.log(this.buyData)
+      console.log(this.buyData);
       var timestamp = this.buyData.timestamp;
-      console.log("timestamp:",timestamp)
+      console.log("timestamp:", timestamp);
       const flight_no = this.buyData.flight_no;
       await this.NewPolicy(timestamp, flight_no);
-
     },
-
   },
   watch: {
     show(val) {
